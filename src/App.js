08a874acde6e40b442609b8ea584b17c7a4fa96d
gianella.meringolo <gmeringolo@gmail.com>
2022-12-07 20:16:17 -0300

    initial config

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  BrowserRouter  as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home";
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          { /* Es muy recomendable añadir esta ruta para obtener un mensaje de error en el caso de que la ruta no exista. De lo contrario, si la ruta no existe llegaremos a una página en blanco     
+          <Route path="*" element={<div>404</div> } />*/}   
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
