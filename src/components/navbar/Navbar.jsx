47b449b72215918f4f3aab6198f1cba95220e2d0
gianella.meringolo <gmeringolo@gmail.com>
2022-12-07 23:47:29 -0300

    navbar v1

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from "./navbar.module.scss";
+import logo from "./LogoNav.png";
+import exit from "./exit.png";
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className={styles.menu}>
+          <a href="#inicio" className={styles.enlace}>
+            <img src={logo} className={styles.logo} alt="logo Guruyu" />
+          </a>
+          <ul>
+            <li>
+              <a href="#jugadores" title="jugadores">
+                Jugadores
+              </a>
+            </li>
+            <li>
+              <a href="#equipos" title="equipos">
+                Equipos
+              </a>
+            </li>
+            <li>
+              <a href="#partidos" title="partidos">
+                Partidos
+              </a>
+            </li>
+            <li>
+              <a href="#medios" title="medios">
+                Medios
+              </a>
+            </li>
+            <li>
+              <a href="#noticias" title="noticias">
+                Noticias
+              </a>
+            </li>
+            <li>
+              <a href="#f&q" title="f&q">
+                F&Q
+              </a>
+            </li>
+          </ul>
+        </div>
+        <img src={exit} className={styles.exit} alt="cerrar sesión" />
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-//import Navbar from "../components/navbar/Navbar";
+import Navbar from "../components/navbar/Navbar";
 
 function home() {
   return (
     <>
-
+      <Navbar />
     </>
   );
 }
